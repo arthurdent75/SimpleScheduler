d5a9dfe77742c2e8d9b901fd5b61f17f5eb446d0
arthurdent75 <60071192+arthurdent75@users.noreply.github.com>
2020-02-27 21:56:29 +0100

    Update conf.php

--- web/conf.php
+++ web/conf.php
@@ -6,5 +6,5 @@ $json_folder = "/share/simplescheduler/";
 $SUPERVISOR_TOKEN = getenv('SUPERVISOR_TOKEN');
 $HASSIO_URL = "http://hassio/homeassistant/api";
 $options_json_filename = "/data/options.json";
-$sun_filename = $json_folder."/sun.dat";
+$sun_filename = $json_folder."sun.dat";
 
