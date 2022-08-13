b278d60b842f3758d5d4fad65b1637574e28359e
Michael Wyraz <michael@wyraz.de>
2022-08-13 19:33:42 +0200

    Make HASSIO_URL configurable by env variable

--- rootfs/var/www/html/conf.php
+++ rootfs/var/www/html/conf.php
@@ -4,7 +4,7 @@
 
 $json_folder = "/share/simplescheduler/";
 $SUPERVISOR_TOKEN = getenv('SUPERVISOR_TOKEN');
-$HASSIO_URL = "http://hassio/homeassistant/api";
+$HASSIO_URL = getenv('HASSIO_URL') ?: "http://hassio/homeassistant/api";
 $options_json_file = $json_folder."options.dat";
 $sun_filename = $json_folder."sun.dat";
 
